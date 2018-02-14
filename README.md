7c3901b08edb8770845c9196ac335aae9606953f
Sebastian Fitzner <sebastian.fitzner@aperto.com>
2018-02-14 11:23:15 +0100

    Add http service with readme and example

--- README.md
+++ README.md
@@ -1,1 +1,121 @@
-# http-service
+<p align="center"><img src="https://www.veams.org/img/svg/icons/veams-std.svg"></p>
+<p align="center">Head to the <a href="http://veams.org/">site</a> for detailed instructions.
+<br><br>
+<a href="https://gitter.im/Sebastian-Fitzner/Veams?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge"><img src="https://badges.gitter.im/Sebastian-Fitzner/Veams.svg" alt="Gitter Chat" /></a></p>
+
+
+# HTTP Service (`@veams/http-service`)
+
+Simple http service provided for and by Veams. 
+
+## Installation
+
+Install the package: 
+
+### NPM
+
+```bash
+npm install @veams/http-service --save 
+```
+
+### Yarn
+
+```bash
+yarn add @veams/http-service 
+```
+
+## Usage
+
+You can simple use the http service and modify it for your needs.
+
+### Lifecycle Hooks
+
+You can intercept into the request by using the following methods: 
+
+- `requestWillOpen(request: XMLHttpRequest, obj: any): void;`
+- `requestDidOpen(request: XMLHttpRequest, obj: any): void;`
+- `requestWillLoad(request: XMLHttpRequest, obj: any): void;`
+- `requestDidLoad(request: XMLHttpRequest, obj: any): void;`
+- `requestWillSend(request: XMLHttpRequest, obj: any): void;`
+- `requestDidSend(request: XMLHttpRequest, obj: any): void;`
+
+As you can see, each method gets the request object which you can modify like you wish.
+
+### Methods
+
+You can execute the typical `CRUD` methods: 
+- `get(url?: any): Promise<{}>;`
+- `delete(url?: any): Promise<{}>;`
+- `post(url: any, data: any): Promise<{}>;`
+- `put(url: any, data: any): Promise<{}>;`
+
+Each method returns a Promise. Be sure you have a polyfill included!
+
+### Parser
+
+The default parser returns the `responseText` as string or object. This is dependent on the type you have defined. 
+
+You can easily override the parser by taking a look at the example. 
+
+### Example
+
+```js 
+import HttpService from '@veams/http-service';
+
+let httpService = new HttpService({
+	type: 'json'
+});
+
+/** 
+ * Override the default parser,
+ * in which we want to return more than the responseText
+ */
+httpService.parser = ({ request }) => {
+	return {
+		status: request.status,
+		statusText: request.statusText,
+		body: JSON.parse(request.responseText)
+	};
+};
+
+class MyPagesService {
+	url = 'http://localhost:3000/api/pages';
+	http = httpService;
+
+	/**
+	 * Static id checker.
+	 *
+	 * @param {String} id - Id of the endpoint.
+	 */
+	static checkId(id) {
+		if (!id || typeof id !== 'string') {
+			throw new Error(`PagesService :: You have to provide an "id" and this "id" needs to be a string!`);
+		}
+	}
+
+	/**
+	 * Fetch data items from the endpoint.
+	 */
+	read() {
+		return this.http.get(`${this.url}`);
+	}
+
+	/**
+	 * Fetch data item by provided id from the endpoint.
+	 *
+	 * @param {String} id - Id of the endpoint.
+	 */
+	readById(id) {
+		this.constructor.checkId(id);
+
+		return this.http.get(`${this.url}/${id}`);
+	}
+}
+
+const myPagesService = new MyPagesService();
+
+export default myPagesService;
+```
+
+
+That's it!
---
+++ src/index.ts
@@ -0,0 +1,208 @@
+/**
+ * Represents a simple http service, which returns a promise.
+ *
+ * @module http
+ * @author Sebastian Fitzner
+ */
+
+// Imports
+import Base from '@veams/base';
+
+export interface RequestObject {
+	method?: string;
+	url?: string;
+	type?: string;
+	data?: string;
+}
+
+export interface ServiceOptions {
+	url?: string,
+	type?: string,
+	method?: string,
+	fetchOnInit?: boolean,
+	headers?: object
+}
+
+export interface Parser {
+	type: string,
+	request: Request
+}
+
+export interface Request {
+	responseText: string
+}
+
+class HttpService extends Base {
+	data: any;
+	options: ServiceOptions;
+
+	constructor(opts: ServiceOptions = {}) {
+		let namespace = '@veams/http-service';
+		let defaultOptions = {
+			url: null,
+			type: 'text',
+			method: 'GET',
+			fetchOnInit: false,
+			headers: null
+		};
+
+		super({namespace, opts}, defaultOptions);
+
+		this.data = {};
+		this.initialize();
+	};
+
+	initialize() {
+		if (!window['Promise']) {
+			console.error('@veams/http-service :: You should add a lightweight promise library like promise-polyfill!');
+		}
+
+		if (this.options.fetchOnInit) {
+			return this.promiseRequest();
+		}
+	};
+
+	// Request lifecycle
+	requestWillOpen(request: XMLHttpRequest, obj) {
+	}
+
+	requestDidOpen(request: XMLHttpRequest, obj) {
+		if (this.options.headers) {
+			for (let header in this.options.headers) {
+				if (this.options.headers.hasOwnProperty(header)) {
+					request.setRequestHeader(header, this.options.headers[header]);
+				}
+			}
+		}
+	}
+
+	requestWillLoad(request: XMLHttpRequest, obj) {
+	}
+
+	requestDidLoad(request: XMLHttpRequest, obj) {
+	}
+
+	requestWillSend(request: XMLHttpRequest, obj) {
+	}
+
+	requestDidSend(request: XMLHttpRequest, obj) {
+	}
+
+	// Request function
+	promiseRequest(obj?: RequestObject) {
+		return new Promise((resolve, reject) => {
+			let request = new XMLHttpRequest();
+			let data = obj.type === 'json' ? JSON.stringify(obj.data) : obj.data;
+
+			this.requestWillOpen(request, obj);
+			request.open(obj.method, obj.url, true);
+			this.requestDidOpen(request, obj);
+
+			this.requestWillLoad(request, obj);
+			request.onload = () => {
+				if (request.status >= 200 && request.status < 400) {
+					resolve(this.parser({
+						request: request,
+						type: obj.type
+					}));
+
+					this.requestDidLoad(request, obj);
+				} else {
+					reject({
+						status: request.status,
+						statusText: request.statusText
+					});
+
+					this.requestDidLoad(request, obj);
+				}
+			};
+
+			request.onerror = function () {
+				reject({
+					status: request.status,
+					statusText: request.statusText
+				});
+			};
+
+			this.requestWillSend(request, obj);
+
+			request.send(data);
+			this.requestDidSend(request, obj);
+		});
+	};
+
+	get(url = null) {
+		let requestObject: RequestObject = {};
+
+		this.options.method = requestObject.method = 'GET';
+		this.options.url = requestObject.url = url || this.options.url;
+		this.options.type = requestObject.type = this.options.type;
+
+		return this.promiseRequest(requestObject);
+	};
+
+	delete(url = null) {
+		let requestObject: RequestObject = {};
+
+		requestObject.method = 'DELETE';
+		requestObject.url = url || this.options.url;
+		// requestObject.type = this.options.type;
+
+		return this.promiseRequest(requestObject);
+	}
+
+	post(url = null, data) {
+		let requestObject: RequestObject = {};
+
+		requestObject.data = data ? data : null;
+		requestObject.method = 'POST';
+		requestObject.url = url || this.options.url;
+		requestObject.type = this.options.type;
+
+		if (this.options.type === 'json' && this.options.headers === null) {
+			this.options.headers = {
+				'content-type': 'application/json'
+			};
+		}
+
+		return this.promiseRequest(requestObject);
+	}
+
+	put(url = null, data) {
+		let requestObject: RequestObject = {};
+
+		requestObject.data = data ? data : null;
+		requestObject.method = 'PUT';
+		requestObject.url = url || this.options.url;
+		requestObject.type = this.options.type;
+
+		if (this.options.type === 'json' && this.options.headers === null) {
+			this.options.headers = {
+				'content-type': 'application/json'
+			};
+		}
+
+		return this.promiseRequest(requestObject);
+	}
+
+	/**
+	 * The default parser, which returns the response text.
+	 * This method can be overridden.
+	 *
+	 * @param {Object} obj - Generic object.
+	 * @param {Object} obj.request - Request object.
+	 * @param {String} obj.type - Define a type for the response text.
+	 * @param {Object} obj.data - Data object.
+	 */
+	parser(obj: Parser): object {
+		this.data = obj.request.responseText;
+
+		if (obj.type === 'json') {
+			this.data = JSON.parse(this.data);
+		}
+
+		return this.data;
+	}
+}
+
+export default HttpService;
